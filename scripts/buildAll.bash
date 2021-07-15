e8483e0f69436cba2b38fa310bc175917fdb09d7
Knarfux V <knarfux@system76-pc.localdomain>
2021-07-15 08:17:14 -0400

    Update stack

--- scripts/buildAll.bash
+++ scripts/buildAll.bash
@@ -11,3 +11,11 @@ docker build -t be-project .
 echo "Building auth server..."
 cd ../BE-Project-authentication || exit 1
 docker build -t be-project-authentication .
+
+echo "Building lobby server..."
+cd ../BE-Project-lobby || exit 1
+docker build -t be-project-lobby .
+
+echo "Building game server..."
+cd ../BE-Project-game || exit 1
+docker build -t be-project-game .
